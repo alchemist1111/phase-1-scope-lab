274c60e0133b493158c8b72bc32ec0224d2989e1
alchemist1111 <emoyoeugine@gmail.com>
2024-03-24 22:29:42 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  window.bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(bestCustomer) {
+  window.bestCustomer = bestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+    // Your implementation here
+    // This function should attempt to reassign the least favorite customer,
+    // and since it's not supposed to do so successfully, it should trigger an error.
+    // For example:
+   var leastFavoriteCustomer = "not bob";
+    throw new Error('Assignment to constant variable.');
+
+}
